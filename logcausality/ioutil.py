f1b1b82df7c5838290d785fa3ade81ab28227cf3
sat <sat@hongo.wide.ad.jp>
2015-12-16 19:36:05 +0900

    update ioutil.py

--- logcausality/ioutil.py
+++ logcausality/ioutil.py
@@ -5,14 +5,16 @@
 def table(data, label = None, spl = " ", fill = " "):
     # data : [row = [str, str, ...], row1, ...]
 
+    l_row = data
+    if label is not None:
+        l_row = [label] + data
     l_length = [max(column, key = lambda x: len(str(x)))
             for column in zip(data)]
+
     l_buf = []
-    
     if label is not None:
         l_buf.append(spl.join([elem.ljust(length)
                 for elem, length in zip(label, l_length)]))
-
     for row in data:
         temp_buf = []
         for elem, length in zip(row, l_length):
